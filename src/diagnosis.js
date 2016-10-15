482c60e289a7c5f71fefda8400fae60c35cb672e
buttegab <gabriel.butterick@students.olin.edu>
2016-10-15 16:27:51 -0400

    now saves setting to crome

--- src/diagnosis.js
+++ src/diagnosis.js
@@ -1,6 +1,4 @@
-console.log("blarg");
 var diagnose_func = function () {
-  console.log("trying");
   $( "#diagnose" ).submit(function( event ) {
     event.preventDefault();
 
@@ -10,12 +8,69 @@ var diagnose_func = function () {
     var d = $('input[name=color4]:checked', '#diagnose').val();
     var e = $('input[name=color5]:checked', '#diagnose').val();
     var f = $('input[name=color6]:checked', '#diagnose').val();
-    var res = [a,b,c];
-    alert(res);
     
+    
+    chrome.storage.sync.set({
+      Color1: a,
+      Color2: b,
+      Color3: c,
+      Color4: d,
+      Color5: e,
+      Color6: f
+  }, function() {
+      // Send alert to user to verify saving.
+      alert("Saved");
+  });
+
+
   });
 };
 $(document).ready(function() {
   diagnose_func()
 });
 
+// // Saves options to chrome.storage
+// function save_options() {
+//   var color = document.getElementById('color').value;
+//   var likesColor = document.getElementById('like').checked;
+//   chrome.storage.sync.set({
+//     favoriteColor: color,
+//     likesColor: likesColor
+//   }, function() {
+//     // Update status to let user know options were saved.
+//     var status = document.getElementById('status');
+//     status.textContent = 'Options saved.';
+//     setTimeout(function() {
+//       status.textContent = '';
+//     }, 750);
+//   });
+// }
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  // Use default value color = 'red' and likesColor = true.
+  chrome.storage.sync.get({
+    // favoriteColor: 'red',
+    // likesColor: true
+    Color1: 'Distinct',
+    Color2: 'Distinct',
+    Color3: 'Distinct',
+    Color4: 'Distinct',
+    Color5: 'Distinct',
+    Color6: 'Distinct'
+
+  }, function(items) {
+    // document.getElementById('color').value = items.favoriteColor;
+    // document.getElementById('like').checked = items.likesColor;
+    $('input[name=color1]:checked', '#diagnose').val(items.Color1);
+    $('input[name=color2]:checked', '#diagnose').val(items.Color2);
+    $('input[name=color3]:checked', '#diagnose').val(items.Color3);
+    $('input[name=color4]:checked', '#diagnose').val(items.Color4);
+    $('input[name=color5]:checked', '#diagnose').val(items.Color5);
+    $('input[name=color6]:checked', '#diagnose').val(items.Color6);
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+// document.getElementById('save').addEventListener('click',
+//     save_options);
