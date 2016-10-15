e71abe204e226cd0465b65f44ab8a275a26ec609
buttegab <gabriel.butterick@students.olin.edu>
2016-10-15 19:33:19 -0400

    color-blindness now saved as object

--- src/diagnosis.js
+++ src/diagnosis.js
@@ -1,4 +1,3 @@
-//todo: instead of storing results of options, analyze results and store that
 var diagnose_func = function () {
   $( "#diagnose" ).submit(function( event ) {
     event.preventDefault();
@@ -18,25 +17,19 @@ var diagnose_func = function () {
         res[i] = true
       }
     }
-
-
-
-
+    var blindness = {dGreen_Red: res[0], dYellow_Blue: res[1], lRed_Green: res[2], Magenta_dCyan: res[3], Red_Green: res[4], Yellow_Blue: res[5]}; 
     chrome.storage.sync.set({
-      dGreen_Red: res[0],
-      dYellow_Blue: res[1],
-      lRed_Green: res[2],
-      Magenta_dCyan: res[3],
-      Red_Green: res[4],
-      Yellow_Blue: res[5]
+      ColorBlindness: blindness
+      // dGreen_Red: res[0],
+      // dYellow_Blue: res[1],
+      // lRed_Green: res[2],
+      // Magenta_dCyan: res[3],
+      // Red_Green: res[4],
+      // Yellow_Blue: res[5]
   }, function() {
       // Send alert to user to verify saving.
-      // alert("Saved");
-      alert(res);
-
-  });
-
-
+      alert("Saved");
+    });
   });
 };
 $(document).ready(function() {
@@ -45,16 +38,26 @@ $(document).ready(function() {
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
-function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get({
-  }, function(items) {
-    $('input[name=color1]:checked', '#diagnose').val(items.dGreen_Red);
-    $('input[name=color2]:checked', '#diagnose').val(items.dYellow_Blue);
-    $('input[name=color3]:checked', '#diagnose').val(items.lRed_Green);
-    $('input[name=color4]:checked', '#diagnose').val(items.Magenta_dCyan);
-    $('input[name=color5]:checked', '#diagnose').val(items.Red_Green);
-    $('input[name=color6]:checked', '#diagnose').val(items.Yellow_Blue);
-  });
-}
-document.addEventListener('DOMContentLoaded', restore_options);
+
+// function restore_options() {
+//   // Use default value color = 'red' and likesColor = true.
+//   alert("restoring");
+//   chrome.storage.sync.get({
+//   }, function(items) {
+//     res = [items.dGreen_Red,items.dYellow_Blue,items.lRed_Green,items.Magenta_dCyan,items.Red_Green,items.Yellow_Blue]
+//     for (i = 0; i < res.length; i++) {
+//       if (res[i] == false) {
+//         res[i] = "Distinct"
+//       } else {
+//         res[i] = "Indistinct"
+//       }
+//     }
+//     $('input[name=color1]', '#diagnose').val(res[0]);
+//     $('input[name=color2]', '#diagnose').val(res[1]);
+//     $('input[name=color3]', '#diagnose').val(res[2]);
+//     $('input[name=color4]', '#diagnose').val(res[3]);
+//     $('input[name=color5]', '#diagnose').val(res[4]);
+//     $('input[name=color6]', '#diagnose').val(res[5]);
+//   });
+// }
+// document.addEventListener('DOMContentLoaded', restore_options);
