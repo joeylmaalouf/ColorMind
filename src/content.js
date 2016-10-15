85abbb5b91057f67cd75ed5d53b915306dea065d
dbishop <dbishop.rwc@gmail.com>
2016-10-15 15:39:49 -0400

    rgb values properly incremented

--- src/content.js
+++ src/content.js
@@ -65,7 +65,7 @@ function applyStyles(){};
 	function getColor(color) {
 		c = colorMap[color.toString()];
 		if (c == undefined) {
-			c = color[0] + 50 + ',' + color[1] + 50 + "," + color[2] + 50;
+			c = parseInt(color[0] + 50) + ',' + parseInt(color[1] + 50) + "," + parseInt(color[2] + 50);
 			colorMap[color.toString] = c;
 		}
 		c = c.split(',');
